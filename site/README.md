c991697fd69cc2e5705d6e254774abf7b05dfb91
Kier Davis <kierdavis@gmail.com>
2012-08-06 13:39:31 +0100

    Added website folder

---
+++ site/README.md
@@ -0,0 +1,1 @@
+This folder contains the server directory for Apache
---
+++ site/index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Where do you want to live?</title>
+    </head>
+    
+    <body>
+        <h1>Testing...</h1>
+        <p>Seems to be working</p>
+    </body>
+</html>
+
