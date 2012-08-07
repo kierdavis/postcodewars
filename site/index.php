d7e9d9ff70680ac98c365f8f9c3f6afc14ba5661
Blooguardian <edward@vaudin.net>
2012-08-07 15:22:16 +0200

    Pasted in alternate page.

--- site/index.php
+++ site/index.php
@@ -7,35 +7,70 @@ keep files seperate:
 -->
 
 <html>
-    <head>
-        <title>Where do you want to live?</title>
-        
-        <link rel="stylesheet" type="text/css" href="/static/css/style.css"/>
-        <link rel="stylesheet" type="text/css" href="/static/css/index.css"/>
-        
-        <script type="text/css" src="/static/js/jquery-1.7.2.min.js"></script>
-        <script type="text/css" src="/static/js/modernizr.js"></script>
-        <script type="text/css" src="/static/js/global.js"></script>
-        <script type="text/css" src="/static/js/index.js"></script>
-    </head>
-    
-    <body>
-        <div id="header">
-            <img src="/static/images/header.png"/>
+<head>
+    <title>RateMyArea</title>
+    <style type="text/css">
+        #Text1
+        {
+            width: 522px;
+            height: 25px;
+        }
+        input.textbox
+        {
+            border:3px solid #000000;
+            text-align: center;
+            padding:5px;
+             -webkit-border-radius: 4px;
+       -moz-border-radius: 4px;
+            border-radius: 4px;
+        }
+        input.textbox:focus 
+        {
+            border:5px
+            outline:none;
+            border-color:#96FFFF;
+            -webkit-box-shadow: 0px 0px 6px #000000;
+        }
+        input.button
+        {
+            width: 120px;
+            height: 30px;
+        } 
+        .style1
+        {
+            width: 412px;
+            height: 166px;
+        }
+        .style2
+        {
+            color: #FF0000;
+            font-family: Courier;
+        }
+    </style>
+</head>
+<body style="font-family: 'Segoe UI'; font-weight: 700; color: #0000CC; text-align: center; font-size: large;" 
+    background="small-houses.jpg">
+        <p style="color: #FFFFFF; font-family: Courier; text-align: left;">
+            RateMyArea: Alpha 0.1.4
+                <center><img alt="" class="style1" src="Capture.PNG" /></center>Type in your 
+        postcode to get started.</p>
+    <form id="textBoxInput"><center><input id="Text1" type="text" class="textbox" style="font-family: Arial Black; font-size: 16px;" /><br />
+        </center></form>
+       
+
+   <br />
+   <br />
+        <a href="postcodeConfirm.htm"><center><input id="search" type="button" class="button" value="Search" /></center></a>
+        <p>
+        &nbsp;</p>
+        <p>
+        &nbsp;</p>
+        <p>
+        &nbsp;</p>
+        <div style="font-family: Courier; color: #FFFFFF">
+            <p class="style2" style="text-align: center; font-size: medium">
+                &nbsp;</p>
         </div>
-        
-        <div id="body">
-            <form action="" method="get" class="nosubmit-form">
-                <label for="postcode-field">Postcode:</label>
-                <input type="text" name="postcode" id="postcode-field" size="8" placeholder="Postcode"/>
-                
-                <!-- TODO: remove "Search" text and add background image -->
-                <a href="#" id="search-button">Search</a>
-            </form>
-        </div>
-        
-        <div id="footer">
-            
-        </div>
-    </body>
+
+</body>
 </html>
