d35d16c871ded050fb84d303d2a3c89e7718ed26
Ben Hayman <ben@bnhymn.com>
2012-08-07 18:12:17 +0100

    New background and basic javascript button implementation

--- site/static/js/global.js
+++ site/static/js/global.js
@@ -1,31 +1,13 @@
 $(document).ready(function() {
-    $(".nosubmit-form").submit(function(event) {
-        event.preventDefault(); // Is this right?
-        return false;
-    });
+	$("#battle_submit").click(function(event) {
+		event.preventDefault();
 
-    // Pasted from http://webdesignerwall.com/tutorials/cross-browser-html5-placeholder-text
-    if(!Modernizr.input.placeholder){
-        $('[placeholder]').focus(function() {
-          var input = $(this);
-          if (input.val() == input.attr('placeholder')) {
-            input.val('');
-            input.removeClass('placeholder');
-          }
-        }).blur(function() {
-          var input = $(this);
-          if (input.val() == '' || input.val() == input.attr('placeholder')) {
-            input.addClass('placeholder');
-            input.val(input.attr('placeholder'));
-          }
-        }).blur();
-        $('[placeholder]').parents('form').submit(function() {
-          $(this).find('[placeholder]').each(function() {
-            var input = $(this);
-            if (input.val() == input.attr('placeholder')) {
-              input.val('');
-            }
-          })
-        });
-    }
+		$("#search").after($('<div class="loader"></div>'));
+
+		var url = "/results-json.php?postcode1=" + "&postcode2=";
+
+		$.getJSON(url, function(data) {
+			console.log(data);
+		});
+	});
 });
