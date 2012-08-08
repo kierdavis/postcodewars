f754d8779ea9b2d6ca8db9fe3ef08beccdcf551f
Ben Hayman <ben@bnhymn.com>
2012-08-08 13:05:33 +0100

    Readded include THIS IS THE WORST IDEA EVER

---
+++ lib/include.php
@@ -0,0 +1,17 @@
+<?php
+    // Show errors to the screen for now - comment out when in production
+    //ini_set("display_errors", "stdout");
+    
+    define("GOOGLE_API_KEY", "AIzaSyAlxfcq-vCtqP4Z7xjPJTHgWzX1T17TlfU");
+    
+    define("LOWER_IS_BETTER", 0);
+    define("HIGHER_IS_BETTER", 1);
+    
+    // Whether a point should be awarded to both sies in the event of a draw
+    define("GET_POINT_ON_DRAW", 0);
+    
+    define("MYSQL_HOST", "localhost");
+    define("MYSQL_USER", "yrs2012app-user");
+    define("MYSQL_PASSWD", "vOdQ04wDTtIS3GeylBER1nNrAo76ZLFJ");
+    define("MYSQL_DB", "yrs2012app");
+?>
