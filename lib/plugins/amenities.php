16a2b37d006a9d29a9beb2660ae653b5b513c5bf
Ben Hayman <ben@bnhymn.com>
2012-08-08 13:27:32 +0100

    Fixed unmerged errors

--- lib/plugins/amenities.php
+++ lib/plugins/amenities.php
@@ -4,18 +4,8 @@
 	class ArtGalleries {
 		public $category = "amenities";			// Category identifier - lowercase and hyphen-separated
 		public $name = "art-galleries-nearby";	// Name identifier - ""
-<<<<<<< HEAD
-<<<<<<< HEAD
-		public $hrname = "Art galleries";		// Displayed in results table
-		public $units = "w/in 2km";				// Units of the results
-=======
 		public $hrname = "Art galleries (within 2km)";		// Displayed in results table
 		public $units = "";				// Units of the results
->>>>>>> fe81428a31be2980a1461305acdf296d0424242b
-=======
-		public $hrname = "Art galleries";		// Displayed in results table
-		public $units = "w/in 2km";				// Units of the results
->>>>>>> c0bc63283f57bbfc65f9b5187dd598db49b53c5c
 		public $better = HIGHER_IS_BETTER;		// LOWER_IS_BETTER or HIGHER_IS_BETTER - determines winner
 		public $can_cache = FALSE;					// If results are cached or not
 		
@@ -34,18 +24,8 @@
 	class Bars {
 		public $category = "amenities";
 		public $name = "bars-nearby";
-<<<<<<< HEAD
-<<<<<<< HEAD
-		public $hrname = "Bars";
-		public $units = "w/in 500m";
-=======
 		public $hrname = "Bars (within 500m)";
 		public $units = "";
->>>>>>> fe81428a31be2980a1461305acdf296d0424242b
-=======
-		public $hrname = "Bars";
-		public $units = "w/in 500m";
->>>>>>> c0bc63283f57bbfc65f9b5187dd598db49b53c5c
 		public $better = HIGHER_IS_BETTER;
 		public $can_cache = FALSE;
 		
@@ -61,18 +41,8 @@
 	class Museums {
 		public $category = "amenities";
 		public $name = "museums-nearby";
-<<<<<<< HEAD
-<<<<<<< HEAD
-		public $hrname = "Museums";
-		public $units = "w/in 2km";
-=======
 		public $hrname = "Museums (within 2km)";
 		public $units = "";
->>>>>>> fe81428a31be2980a1461305acdf296d0424242b
-=======
-		public $hrname = "Museums";
-		public $units = "w/in 2km";
->>>>>>> c0bc63283f57bbfc65f9b5187dd598db49b53c5c
 		public $better = HIGHER_IS_BETTER;
 		public $can_cache = FALSE;
 		
@@ -88,18 +58,8 @@
 	class Restaurants {
 		public $category = "amenities";
 		public $name = "restaurants-nearby";
-<<<<<<< HEAD
-<<<<<<< HEAD
-		public $hrname = "Restaurants";
-		public $units = "w/in 500m";
-=======
 		public $hrname = "Restaurants (within 500m)";
 		public $units = "";
->>>>>>> fe81428a31be2980a1461305acdf296d0424242b
-=======
-		public $hrname = "Restaurants";
-		public $units = "w/in 500m";
->>>>>>> c0bc63283f57bbfc65f9b5187dd598db49b53c5c
 		public $better = HIGHER_IS_BETTER;
 		public $can_cache = FALSE;
 		
