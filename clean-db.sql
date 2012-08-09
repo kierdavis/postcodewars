87c81cd56a8e90532db8d4d1b49fdc18a770065f
Kier Davis <kierdavis@gmail.com>
2012-08-09 12:11:26 +0100

    Fixed errors in HousePriceRise-Fall

--- clean-db.sql
+++ clean-db.sql
@@ -1,8 +1,6 @@
 # This script is run every night on the database
 # DO NOT EDIT unless you know what you're doing.
 
-USE yrs2012app;
-
 # Clean invalid postcodes out of the lookup
 DELETE FROM postcodes WHERE lat = 0 AND lng = 0;
 SELECT CONCAT("Deleted ", ROW_COUNT(), " invalid postcodes") AS "";
