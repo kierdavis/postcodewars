5beddf7e88f6cc87ae0f39088ea184b0b7608316
Kier Davis <kierdavis@gmail.com>
2012-08-09 13:45:12 +0100

    Fixed typo in clearcache.php

--- site/clearcache.php
+++ site/clearcache.php
@@ -1,7 +1,7 @@
 <?php
     require_once "../lib/include.php";
     
-    $db = new mysqli(MYSQL_HOST, MYSQL_USER, MYSQL_PASSWD, MSQL_DB);
+    $db = new mysqli(MYSQL_HOST, MYSQL_USER, MYSQL_PASSWD, MYSQL_DB);
     $res = $db->query("DELETE FROM cache");
     if ($res === FALSE) {
         echo "MySQL Error: " . $db->error . "\n";
