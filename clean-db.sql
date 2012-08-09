ff3a6f44e27d9d107985e51e9eafcdb3eff64286
Kier Davis <kierdavis@gmail.com>
2012-08-09 11:45:40 +0100

    Database cleaning script now has output

--- clean-db.sql
+++ clean-db.sql
@@ -5,6 +5,8 @@ USE yrs2012app;
 
 # Clean invalid postcodes out of the lookup
 DELETE FROM postcodes WHERE lat = 0 AND lng = 0;
+SELECT CONCAT("Deleted ", ROW_COUNT(), " invalid postcodes") AS "";
 
 # Clean cache entries not accessed for at least a day
 DELETE FROM cache WHERE last_accessed < (UNIX_TIMESTAMP() - 86400);
+SELECT CONCAT("Deleted ", ROW_COUNT(), " expired cache entries") AS "";
