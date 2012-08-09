f5762d82db023e4d5c35db2522538c452ad4ecdf
Jake Patterson <jakepatterson4@gmail.com>
2012-08-09 13:26:54 +0100

    Fixed the place type

--- lib/plugins/artgalleries.php
+++ lib/plugins/artgalleries.php
@@ -34,7 +34,7 @@
         //     "lng" => the longitude
         public function get_result($db, $loc) {
             // Do something with $location
-            $result=get_all_results($loc["postcode"],"","art gallery",$loc["lat"],$loc["lng"],5000);
+            $result=get_all_results($loc["postcode"],"","art_gallery",$loc["lat"],$loc["lng"],5000);
             // Should return a number - this is the result that is displayed.
             $no_of_artgalleries=count($result);
             return $no_of_artgalleries;
