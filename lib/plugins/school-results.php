f52dc1be306b93b79c0009abf3b06b16931f8add
Edward Longman <eialongman@gmail.com>
2012-08-09 14:42:44 +0100

    only searches for primary schools

--- lib/plugins/school-results.php
+++ lib/plugins/school-results.php
@@ -36,8 +36,8 @@
         	
         	//find the nearest school!
 			logmsg("school-results", "first line of schhol results");
-        	$nearest_school=get_nearest_result($loc["postcode"],"","school",$loc["lat"],$loc["lng"]);
-			logmsg("school-results", json_encode($nearest_school));
+        	$nearest_school=get_nearest_result($loc["postcode"],"primary","school",$loc["lat"],$loc["lng"]);
+			logmsg("school-results", "json_data".json_encode($nearest_school));
         	
 			$addr_of_school=$nearest_school->formatted_address;
 			$matches=array();
