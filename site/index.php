d864d862455d1070928a0deaf61e786f77b28036
Edward Vaudin <edward@vaudin.net>
2012-08-09 15:42:01 +0100

    Fixed Cookie Bug

--- site/index.php
+++ site/index.php
@@ -36,10 +36,10 @@
 			<h2>Settings</h2>
 			<p><b>Here you can change what goes to war!</b></p>
 			<ul>
-				<li>Crime Score <input checked id="crimebox" type="checkbox" /></li>
-				<li>School Score <input checked id="schoolbox" type="checkbox" /></li>
-				<li>Amenities Score <input checked id="aebox" type="checkbox" /></li>
-				<li>House Price Score <input checked id="house-prices-visibility" type="checkbox" /></li>
+				<li>Crime Score <input checked id="crime-visibility"" type="checkbox" /></li>
+				<li>School Score <input checked id="schools-visibility" type="checkbox" /></li>
+				<li>Amenities Score <input checked id="amenities-visibility" type="checkbox" /></li>
+				<li>House Price Score <input checked id="house-price-visibility" type="checkbox" /></li>
 			</ul>
 		</div>
 
