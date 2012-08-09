3e8718955c3be9ceb3cb500de89c7d52cee67efa
Kier Davis <kierdavis@gmail.com>
2012-08-09 13:39:12 +0100

    Clearcache shows error

--- site/clearcache.php
+++ site/clearcache.php
@@ -1,6 +1,10 @@
 <?php
     $db = new mysqli(MYSQL_HOST, MYSQL_USER, MYSQL_PASSWD, MSQL_DB);
-    $db->query("DELETE FROM cache");
+    $res = $db->query("DELETE FROM cache");
+    if ($res === FALSE) {
+        echo "MySQL Error: " . $db->error . "\n";
+    }
+    else {
+        echo "Cache cleared.\n"
+    }
 ?>
-
-Cache cleared.
