99e56a4c2a578ae9b20f50f6d680c2918dfb4fb2
Kier Davis <kierdavis@gmail.com>
2012-08-09 13:43:51 +0100

    Added missing include to clearcache.php

--- site/clearcache.php
+++ site/clearcache.php
@@ -1,4 +1,6 @@
 <?php
+    require_once "../lib/include.php";
+    
     $db = new mysqli(MYSQL_HOST, MYSQL_USER, MYSQL_PASSWD, MSQL_DB);
     $res = $db->query("DELETE FROM cache");
     if ($res === FALSE) {
