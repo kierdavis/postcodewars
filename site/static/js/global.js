bd397d284385bb1c7df647cf431095aad9508bee
Edward Vaudin <edward@vaudin.net>
2012-08-09 14:25:45 +0100

    Testing Settings function

--- site/static/js/global.js
+++ site/static/js/global.js
@@ -2,13 +2,17 @@ $(document).ready(function() {
     $(".flip").click(function() {
 		$("#settingsPanel").slideToggle();
 	});
+	
+	$("#settingsPanel input").change(function() {
+		alert("Settings Changed!");
+	});
 		
 	$("#battle_submit").click(function(event) {
 		event.preventDefault();
 		$("#search").after($('<div class="loader"></div>'));
 
 		var url = "/results-json.php?postcode1=" + "&postcode2=";
-
+		
 		$.getJSON(url, function(data) {
 			console.log(data);
 		});
