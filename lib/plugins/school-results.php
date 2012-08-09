2bc15735472b9114fb870b578f3155e2156309be
Edward Longman <eialongman@gmail.com>
2012-08-09 14:01:35 +0100

    added test line at the start

--- lib/plugins/school-results.php
+++ lib/plugins/school-results.php
@@ -35,6 +35,7 @@
         public function get_result($db, $loc) {
         	
         	//find the nearest school!
+			logmsg("school-results", "first line of schhol results");
         	$nearest_school=get_nearest_result($loc["postcode"],"","school",$loc["lat"],$loc["lng"]);
 			$addr_of_school=$nearest_school->formatted_address;
 			$matches=array();
