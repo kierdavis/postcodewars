17f2b2bc5dd8698bf95c9288aad4812da3b5f2d3
Kier Davis <kierdavis@gmail.com>
2012-08-09 11:19:03 +0100

    Re-did cache database structure

--- lib/search.php
+++ lib/search.php
@@ -31,7 +31,7 @@
         
         $postcode_encoded = $db->real_escape_string($location["postcode"]);
         $plugin_encoded = $db->real_escape_string($plugin->name);
-        $res = $db->query("SELECT value FROM cache WHERE postcode = '$postcode_encoded' AND plugin = '$plugin_encoded'");
+        $res = $db->query("SELECT id, value FROM cache WHERE postcode = '$postcode_encoded' AND plugin = '$plugin_encoded'");
         if ($res === FALSE) {
             fwrite($plugin_log, "MySQL error: " . $db->error . "\n");
             return FALSE;
@@ -42,7 +42,16 @@
         }
         
         $row = $res->fetch_row();
-        $v = $row[0];
+        $id = $row[0];
+        $v = $row[1];
+        
+        $last_accessed = $db->real_escape_string(time());
+        $res = $db->query("UPDATE cache SET last_accessed = $last_accessed WHERE id = $id");
+        if ($res === FALSE) {
+            fwrite($plugin_log, "MySQL error: " . $db->error . "\n");
+            return FALSE;
+        }
+        
         return $v;
     }
     
@@ -52,7 +61,8 @@
         $postcode_encoded = $db->real_escape_string($location["postcode"]);
         $plugin_encoded = $db->real_escape_string($plugin->name);
         $result_encoded = $db->real_escape_string($result);
-        $res = $db->query("INSERT INTO cache VALUES ('$plugin_encoded', '$postcode_encoded', $result_encoded)");
+        $last_accessed_encoded = $db->real_escape_string(time());
+        $res = $db->query("INSERT INTO cache VALUES ('$plugin_encoded', '$postcode_encoded', $result_encoded, $last_accessed_encoded, NULL)");
         if ($res === FALSE) {
             fwrite($plugin_log, "MySQL error: " . $db->error . "\n");
             return FALSE;
