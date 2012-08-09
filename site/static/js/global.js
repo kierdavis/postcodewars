93179108391abc337d197380a2494af420545d55
Edward Vaudin <edward@vaudin.net>
2012-08-09 20:25:44 +0100

    Attemped fix of cookies

--- site/static/js/global.js
+++ site/static/js/global.js
@@ -1,34 +1,40 @@
 $(document).ready(function() {
+	//By default the settings tab should be hidden
 	$("#settingsPanel").hide();
-    
-	//$("#settingsPanel ul li").each(function() {
-	var cookieName = $($(this).children()).attr("id").replace("-visibility", "");
-		var value = $.cookie(cookieName); 
+	//FIXME
+	//Cookie does not remember hide/show settings on load
+	//Cookie somehow overides the setting code and shows it on load (by default the settings are meant to be hidden).
+	$("#settingsPanel ul li.children").each(function() {
+		var cookieName = $($(this).children()).attr("id").replace("-visibility", "");
+		var value = $.cookie(cookieName);
 		if (value == true) {
-				$("#" + cookieName).show();
-		}else{
+			$("#" + cookieName).show();
+		} else {
 			$("#" + cookieName).hide();
 		};
-		
+
 	});
-	
+	//When settings button is pressed : Slide transition (hide/show)
 	$(".flip").click(function() {
 		$("#settingsPanel").slideToggle();
 	});
-	
+	//When a setting is changed this function saves that change in cookies
 	$("#settingsPanel input").change(function() {
 		var cookieName = $($(this).children()).attr("id").replace("-visibility", "");
 		var newValue = $(this).is(':checked');
-		$.cookie(cookieName, newValue, { expires: 7, path: '/' });
+		$.cookie(cookieName, newValue, {
+			expires : 7,
+			path : '/'
+		});
 		if (newValue) {
 			$("#" + cookieName).show();
-		}else{
+		} else {
 			$("#" + cookieName).hide();
 		}
 	});
-	
+	//When battle button is pressed it reads the two entered postcodes and loads up the categories and data
 	$("#battle_submit").click(function(event) {
-		// event.preventDefault();
+		event.preventDefault();
 		$("#search").after($('<div class="loader"></div>'));
 
 		var postcode1 = $("#battle_postcode1").text();
