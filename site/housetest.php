3d2ed3a6915b29546e6ae88057f3a0adb4480b93
Taiiwo <tylerzacc@gmail.com>
2012-08-09 20:30:41 +0200

    Update site/housetest.php

--- site/housetest.php
+++ site/housetest.php
@@ -1,10 +1,16 @@
+<html>
+<head title="TEst">
+</head>
+<body>
 <form action="/" method="get">
 <input type="text" name="location" value="Input your location" />
-<input type="submit">
+<input type="submit" />
 </form>
 <?php
 
 $townrefined = $_GET["location"];
+	if ($townrefined != ""){
+		
 			$housepriceunrefined = (file_get_contents("http://api.nestoria.co.uk/api?country=uk&pretty=1&action=metadata&place_name=" . $townrefined . "&encoding=xml"));
 			echo $townrefined;
 			
@@ -13,7 +19,7 @@ $townrefined = $_GET["location"];
 			$xmlfile2 = new SimpleXMLElement($housepriceunrefined);
 			$oldhp = ($xmlfile2->xpath('opt/response/metadata[@metadata_name="avg_4bed_property_buy_monthly"]/data[@name="2011_m2"]/@avg_price'));
 			$newhp = ($xmlfile2->xpath('opt/response/metadata[@metadata_name="avg_4bed_property_buy_monthly"]/data[@name="2012_m2"]/@avg_price'));
-			
+	}	
             logmsg("price-rise-fall-percentage", $housepriceunrefined);
             
 			if ($oldhp[0] >= $newhp[0]) {
@@ -26,3 +32,4 @@ $townrefined = $_GET["location"];
             // Should return a number - this is the result that is displayed.
             echo $result;
 ?>
+</body>
