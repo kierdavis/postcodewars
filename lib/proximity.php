ce98db82366907d9fb5246ded776f95cff16b8fe
Edward Longman <eialongman@gmail.com>
2012-08-09 15:13:43 +0100

    changed it back to text search

--- lib/proximity.php
+++ lib/proximity.php
@@ -3,8 +3,8 @@
 	require_once "include.php";
 	function get_all_results($postcode,$criteria,$type,$lat,$lng,$radius){
 	    $c = curl_init();
-		$url="https://maps.googleapis.com/maps/api/place/search/json";
-		$argstr="?types=".$type."&sensor=false&key=".GOOGLE_API_KEY;
+		$url="https://maps.googleapis.com/maps/api/place/textsearch/json";
+		$argstr="?query=".$criteria."&types=".$type."&sensor=false&key=".GOOGLE_API_KEY;
 		$argstr.="&location=".$lat.",".$lng."&radius=".$radius;
         curl_setopt($c, CURLOPT_URL, $url . $argstr);
         curl_setopt($c, CURLOPT_RETURNTRANSFER, TRUE);
