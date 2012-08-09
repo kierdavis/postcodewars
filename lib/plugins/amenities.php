1098f4011294d965a4e5333200f95d1b9ad6660d
Jake Patterson <jakepatterson4@gmail.com>
2012-08-09 14:48:29 +0100

    Moved all amenities into one file, removed others

---
+++ lib/plugins/amenities.php
@@ -0,0 +1,77 @@
+<?php
+	require_once "../lib/proximity.php";
+	
+	class ArtGalleries {
+		public $category = "amenities";						// Category identifier - lowercase and hyphen-separated
+		public $name = "art-galleries";						// Name identifier - ""
+		public $hrname = "Art galleries nearby";	// Displayed in results table
+		public $units = "";												// Units of the results
+		public $better = HIGHER_IS_BETTER;				// LOWER_IS_BETTER or HIGHER_IS_BETTER - determines winner
+		public $can_cache = FALSE;								// If results are cached or not
+		
+		// The get_result method should perform the searches and return the two results.
+		// $db is a mysqli object connected to the database.
+		// $location is an associative array which contains: "postcode", "lat", "lng"
+		public function get_result($db, $loc) {
+			$result = get_all_results($loc["postcode"], "", "art_gallery", $loc["lat"], $loc["lng"], 5000); // Returns number
+			$no_of_art_galleries = count($result);
+			return $no_of_art_galleries;
+		}
+	}
+	
+	// Bars
+	
+	class Bars {
+		public $category = "amenities";
+		public $name = "bars";
+		public $hrname = "Bars nearby";
+		public $units = "";
+		public $better = HIGHER_IS_BETTER;
+		public $can_cache = FALSE;
+		
+		public function get_result($db, $loc) {
+			$result = get_all_results($loc["postcode"], "", "bar", $loc["lat"], $loc["lng"], 5000);
+			$no_of_bars = count($result);
+			return $no_of_bars;
+		}
+	}
+	
+	// Museums
+	
+	class Museums {
+		public $category = "amenities";
+		public $name = "museums";
+		public $hrname = "Museums nearby";
+		public $units = "";
+		public $better = HIGHER_IS_BETTER;
+		public $can_cache = FALSE;
+		
+		public function get_result($db, $loc) {
+			$result = get_all_results($loc["postcode"], "", "museum", $loc["lat"], $loc["lng"], 5000);
+			$no_of_museums = count($result);
+			return $no_of_museums;
+		}
+	}
+	
+	// Restaurants
+	
+	class Restaurants {
+		public $category = "amenities";
+		public $name = "restaurants";
+		public $hrname = "Restaurants nearby";
+		public $units = "";
+		public $better = HIGHER_IS_BETTER;
+		public $can_cache = FALSE;
+		
+		public function get_result($db, $loc) {
+			$result = get_all_results($loc["postcode"], "", "restaurant", $loc["lat"], $loc["lng"], 5000);
+			$no_of_restaurants = count($result);
+			return $no_of_restaurants;
+		}
+	}
+	
+	$plugins["art-galleries"] = new ArtGalleries(); // Inserts the plugin into the plugin index
+	$plugins["bars"] = new Bars();
+	$plugins["museums"] = new Museums();
+	$plugins["restaurants"] = new Restaurants();
+?>
