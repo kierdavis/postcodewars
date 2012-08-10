5460b0925c9129ebe42b10d2b5a1ea6dbb383581
Jake Patterson <jakepatterson4@gmail.com>
2012-08-10 14:35:22 +0100

    Added proximityDist.php - hopefully no functions w/ same name. Changed train/hospital to include proximityDist.php

--- lib/plugins/school-results.php
+++ lib/plugins/school-results.php
@@ -1,37 +1,4 @@
 	<?php
-	function get_all_school_results($postcode,$lat,$lng){
-	    $c = curl_init();
-		$url="https://maps.googleapis.com/maps/api/place/textsearch/json";
-		$argstr="?query=primary+schooltypes=school&sensor=false&key=".GOOGLE_API_KEY;
-		$argstr.="&location=".$lat.",".$lng;
-		if($rankbydist){
-			$argstr.="&rankby=distance";
-		}
-		else{
-			$argstr.="&radius=".$radius;
-		}
-		logmsg("proximity-lib",$url.$argstr);
-        curl_setopt($c, CURLOPT_URL, $url . $argstr);
-        curl_setopt($c, CURLOPT_RETURNTRANSFER, TRUE);
-        $data = curl_exec($c);
-        curl_close($c);
-        $d = json_decode($data);
-		
-        //var_dump($d);
-        return $d->results;
-	}
-	function get_nearest_result($postcode,$criteria,$type,$lat,$lng,$rankbydist){
-        $d=get_all_results($postcode,$criteria,$type,$lat,$lng,"30000",$rankbydist);
-        if (count($d) < 1) {
-            return FALSE;
-        }
-        
-		//gets the lat and long of the first result
-		$endloclat=$d[0]->geometry->location->lat;
-		$endloclng=$d[0]->geometry->location->lng;
-		return array("geo"=>array($endloclat,$endloclng),"name"=>$d[0]->name,"data"=>json_encode($d));
-	}
-	require_once "../lib/proximity.php";
     // Copy this file into lib/plugins/ and name it appropriately. Then follow the comments in this
     // file to fill in the gaps.
 
@@ -65,24 +32,9 @@
         //     "lat" => the latitude
         //     "lng" => the longitude
         public function get_result($db, $loc) {
-        	
-        	//find the nearest school!
-			logmsg("school-results", "first line of school results");
-        	$nearest_school=get_nearest_result($loc["postcode"],"","school",$loc["lat"],$loc["lng"]);
-			logmsg("school-results", "json_data".json_encode($nearest_school));
-        	
-			$addr_of_school=$nearest_school->formatted_address;
-			$matches=array();
-			logmsg("school-results", $addr_of_school);
-			preg_match("[a-zA-Z]{2}[1-9]{1,2}[a-zA-Z]?\s?[1-9]{1}[a-zA-Z]{2}", $addr_of_school, $matches);
-			logmsg("school-results", json_encode($matches));
-			if(!array_key_exists(0, $matches)){
-				return false;
-			};
-        	$postcode_of_school=$matches[0];
             // Do something with $location
 			//TO DO : We need to confirm that the MySQL column names and table name is correct if not then change it!
-            $postcode_encoded = $db->real_escape_string($postcode_of_school);
+            $postcode_encoded = $db->real_escape_string($loc["postcode"]);
 			$queryToSend = "SELECT testscore FROM schools WHERE postcode = \"$postcode_encoded\"";
 			$res = $db->query($queryToSend);
 			logmsg("school-results", "got to line 51");
