a38e86d83aa809a5aee73f24b3db5bf670648522
Jacob Walker <jacob.walker94@gmail.com>
2012-08-10 01:37:57 +0100

    I've found the right $_REQUEST key.

--- site/sms.php
+++ site/sms.php
@@ -1,4 +1,4 @@
 <?php
-	$message = $_REQUEST['From'] . "\r\n" . $_REQUEST['To'] . "\r\n" . $_REQUEST['Message'];
+	$message = $_REQUEST['From'] . "\r\n" . $_REQUEST['To'] . "\r\n" . $_REQUEST['Body'];
 	mail("jacob.walker94@gmail.com", "Twilio tester", $message, "From: twilio-test@androidbanana.co.uk");
 ?>
