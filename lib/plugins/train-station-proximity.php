de3931af274104b512189ed62315d388f91b89a7
Jake Patterson <jakepatterson4@gmail.com>
2012-08-10 15:41:28 +0100

    Updated names for train/hospital proximity - hospital still used a&e names

--- lib/plugins/train-station-proximity.php
+++ lib/plugins/train-station-proximity.php
@@ -4,12 +4,12 @@
     // file to fill in the gaps.
     
     // Change this name
-    class TrainsDist{
+    class TrainStation{
         // The category identifier - should be lowercase and hyphen-separated e.g. "crime"
         public $category = "amenities";
         
         // The name identifier - should be lowercase and hyphen-separated e.g. "school-proximity"
-        public $name = "train-station-proximity"; // internal use - identifies plugin
+        public $name = "train-station-distance"; // internal use - identifies plugin
         
         // The human-readable name - this will be displayed in the results table e.g. "School proximity"
         public $hrname = "Nearest Train Station";
@@ -36,5 +36,5 @@
     
     // Update the name of the class here too.
     // This inserts the plugin into the plugin index.
-    $plugins["trainsdist"] = new TrainsDist();
+    $plugins["trainstation"] = new TrainStation();
 ?>
