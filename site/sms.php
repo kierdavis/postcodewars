d559aded3309d318c0d637429984de6315da8a77
Jacob Walker <jacob.walker94@gmail.com>
2012-08-10 01:31:30 +0100

    I'm making it a bit more helpful

--- site/sms.php
+++ site/sms.php
@@ -1,3 +1,4 @@
 <?php
+	$message = $_REQUEST['From'] . "\r\n" . $_REQUEST['To'] . "\r\n" . $_REQUEST['Message'];
 	mail("jacob.walker94@gmail.com", "Twilio tester", print_r($_REQUEST), "From: twilio-test@androidbanana.co.uk");
 ?>
