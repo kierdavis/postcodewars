f4344d17fca7d014b698c7ba74f92e4723976a23
Jacob Walker <jacob.walker94@gmail.com>
2012-08-10 01:28:35 +0100

    Corrected my email address

--- site/sms.php
+++ site/sms.php
@@ -1,3 +1,3 @@
 <?php
-	mail("jacob.walke94@gmail.com", "Twilio tester", print_r($_REQUEST), "From: twilio-test@androidbanana.co.uk");
+	mail("jacob.walker94@gmail.com", "Twilio tester", print_r($_REQUEST), "From: twilio-test@androidbanana.co.uk");
 ?>
