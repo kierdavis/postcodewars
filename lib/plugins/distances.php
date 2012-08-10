674de5e59968e5f03fec7de44406be40fbcd4c61
Jake Patterson <jakepatterson4@gmail.com>
2012-08-10 16:00:58 +0100

    Quick word change, and added distances.php (forgot before)

---
+++ lib/plugins/distances.php
@@ -0,0 +1,37 @@
+<?php
+	require_once "../lib/proximityDist.php"; // uses different file
+	
+	class Hospitals {
+		public $category = "amenities";			// Category identifier - lowercase and hyphen-separated
+		public $name = "hospital-distance";		// Name identifier - ""
+		public $hrname = "Nearest hospital";	// Displayed in results table
+		public $units = "km";						// Units of the results
+		public $better = LOWER_IS_BETTER;		// LOWER_IS_BETTER or HIGHER_IS_BETTER - determines winner
+		public $can_cache = FALSE;					// If results are cached or not
+		
+		// The get_result method should perform the searches and return the two results.
+		// $db is a mysqli object connected to the database.
+		// $location is an associative array which contains: "postcode", "lat", "lng"
+		public function get_result($db, $loc) {
+			return dist_to_result($loc["postcode"],"hospital",$loc["lat"],$loc["lng"]) / 1000;
+		}
+	}
+	
+	// Train stations
+	
+	class TrainStations {
+		public $category = "amenities";
+		public $name = "train-station-distance";
+		public $hrname = "Nearest train station";
+		public $units = "km";
+		public $better = LOWER_IS_BETTER;
+		public $can_cache = FALSE;
+		
+		public function get_result($db, $loc) {
+			return dist_to_result($loc["postcode"],"train_station",$loc["lat"],$loc["lng"]) / 1000;
+		}
+	}
+	
+	$plugins["hospital-distance"] = new Hospitals(); // Inserts the plugin into the plugin index
+	$plugins["train-station-distance"] = new TrainStations();
+?>
