2b91570d2bf70f2080524a59e3ebaf5c5dd62535
Jacob Walker <jacob.walker94@gmail.com>
2012-08-13 02:20:39 +0100

    Checking for two postcodes and throwing an error otherwise

--- site/sms.php
+++ site/sms.php
@@ -36,44 +36,50 @@ echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 		$message = "Oops! They don't look like postcodes to me.";
 	}
 	else {
-		// Split the two postcodes up
-		$pc1 = $postcodes[0][0];
-		$pc2 = $postcodes[0][1];
-		
-		// Search the postcodes and return scores!
-		$result = search($pc1, $pc2);
-		$score1 = $result['_score1'];
-		$score2 = $result['_score2'];
-
-		// A quick function to split postcodes (or return the unsplit one in some cases) to avoid repetition:
-		function pc_split($pcvar) {
-			if (strlen($pcvar) == 6) {
-				$pcvar_split = str_split($pcvar, 3);
-				$pcvar_imploded = implode(" ", $pcvar_split);
-				return $pcvar_imploded;
+		// First check there are two postcodes
+		if (isset($postcodes[0][0]) && isset($postcodes[0][1])) {
+			// Split the two postcodes up
+			$pc1 = $postcodes[0][0];
+			$pc2 = $postcodes[0][1];
+			
+			// Search the postcodes and return scores!
+			$result = search($pc1, $pc2);
+			$score1 = $result['_score1'];
+			$score2 = $result['_score2'];
+	
+			// A quick function to split postcodes (or return the unsplit one in some cases) to avoid repetition:
+			function pc_split($pcvar) {
+				if (strlen($pcvar) == 6) {
+					$pcvar_split = str_split($pcvar, 3);
+					$pcvar_imploded = implode(" ", $pcvar_split);
+					return $pcvar_imploded;
+				}
+				elseif (strlen($pc1) == 7) {
+					$pcvar_split = str_split($pcvar, 4);
+					$pcvar_imploded = implode(" ", $pcvar_split);
+					return $pcvar_imploded;
+				}
+				else return $pcvar;
 			}
-			elseif (strlen($pc1) == 7) {
-				$pcvar_split = str_split($pcvar, 4);
-				$pcvar_imploded = implode(" ", $pcvar_split);
-				return $pcvar_imploded;
+			
+			// Now do the actual splitting
+			$pc1_split = strtoupper(pc_split($pc1));
+			$pc2_split = strtoupper(pc_split($pc2));
+			
+			// Compare the scores and write an appropriate message
+			if ($score1 > $score2) {
+				$message = strtoupper($pc1_split) . " wins, " . $score1 . "-" . $score2 . "!";
 			}
-			else return $pcvar;
-		}
-		
-		// Now do the actual splitting
-		$pc1_split = strtoupper(pc_split($pc1));
-		$pc2_split = strtoupper(pc_split($pc2));
-		
-		// Compare the scores and write an appropriate message
-		if ($score1 > $score2) {
-			$message = strtoupper($pc1_split) . " wins, " . $score1 . "-" . $score2 . "!";
-		}
-		elseif ($score1 < $score2) {
-			$message = strtoupper($pc2_split) . " wins, " . $score2 . "-" . $score1 . "!";
+			elseif ($score1 < $score2) {
+				$message = strtoupper($pc2_split) . " wins, " . $score2 . "-" . $score1 . "!";
+			}
+			else {
+				$message = "It was a draw!";
+			}
 		}
 		else {
-			$message = "It was a draw!";
-		}
+			$message = "You need two postcodes";
+		}		
 	}
 	
 	// Now output the message as TwiML!
