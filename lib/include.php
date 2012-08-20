3776536cf86fc6593a3efc53c710db2b440a13a1
Jacob Walker <jacob.walker94@gmail.com>
2012-08-20 16:33:23 +0100

    Defined the missing police API key

--- lib/include.php
+++ lib/include.php
@@ -3,6 +3,7 @@
     //ini_set("display_errors", "stdout");
     
     define("GOOGLE_API_KEY", "AIzaSyAlxfcq-vCtqP4Z7xjPJTHgWzX1T17TlfU");
+	define("POLICE_API_KEY", "wejoc29:fc5eeb9a6565be057ee334f7e1665dfb");
     
     define("LOWER_IS_BETTER", 0);
     define("HIGHER_IS_BETTER", 1);
